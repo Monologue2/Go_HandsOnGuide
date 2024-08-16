68779123f53e235d1dda8032ea9abf0af58484e2
shogle <seyoumoon@gmail.com>
2024-08-16 18:41:05 +0900

    create Context with Timeout function

--- exec_context/main.go
+++ exec_context/main.go
@@ -3,10 +3,37 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
 	"time"
 )
+
+func createContextWithTimeout(d time.Duration) (ctx context.Context, cancel context.CancelFunc) {
+	ctx, cancel = context.WithTimeout(context.Background(), d)
+	return ctx, cancel
+}
+
+func setupSignalHandler(w io.Writer, cancelFunc context.CancelFunc) {
+	c := make(chan os.Signal, 1) // os.Signal 을 받는 Buffer 1개를 가진 채널, capacity가 1이다. 라고 표현..
+	// signal package의 Notify 함수는 시스템 호출을 받을 채널을 지정할 수 있다.
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+
+	// Signal 대기를 위한 Go routine, 입력이 올 때까지 대기한다.
+	go func() {
+		s := <-c
+		fmt.Fprintf(w, "Got signal: %v\n", s)
+		cancelFunc()
+	}()
+
+	// Signal 이 제공되지 않는 동안에는 지정된 context 만큼 애플리케이션이 정상적으로 실행된다.
+}
+
+func executeCommand(ctx context.Context, command string, arg string) error {
+	return exec.CommandContext(ctx, command, arg).Run()
+}
 
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
