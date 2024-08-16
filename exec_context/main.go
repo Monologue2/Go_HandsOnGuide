9302a8d6e19d9b432d1a49ce2e907f178c51c8b6
shogle <seyoumoon@gmail.com>
2024-08-16 18:51:00 +0900

    result

--- exec_context/main.go
+++ exec_context/main.go
@@ -36,10 +36,33 @@ func executeCommand(ctx context.Context, command string, arg string) error {
 }
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// defer cancel()
+
+	// if err := exec.CommandContext(ctx, "sleep", "20").Run(); err != nil {
+	// 	fmt.Fprintf(os.Stdout, "%v\n", err)
+	// } // signal: killed
+
+	if len(os.Args) != 3 {
+		fmt.Fprintf(os.Stdout, "Usage: %s <command> <argument>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+	arg := os.Args[2]
+
+	cmdTimeout := 30 * time.Second
+	ctx, cancel := createContextWithTimeout(cmdTimeout)
 	defer cancel()
 
-	if err := exec.CommandContext(ctx, "sleep", "20").Run(); err != nil {
+	setupSignalHandler(os.Stdout, cancel)
+
+	err := executeCommand(ctx, command, arg)
+	if err != nil {
 		fmt.Fprintf(os.Stdout, "%v\n", err)
-	} // signal: killed
+		os.Exit(1)
+	}
+	// $ go run main.go sleep 60
+	// signal: killed
+	// exit status 1
 }
